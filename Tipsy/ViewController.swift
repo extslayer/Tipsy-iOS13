c1ee8ba51c7298124d0bf7e6716be99a2b98a33b
extslayer <kabirismanmohan@gmail.com>
2024-11-12 23:56:33 +0530

    added buttpn

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPcButton: UIButton!
+    @IBOutlet weak var tenPcButton: UIButton!
+    @IBOutlet weak var twentyPcButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tipsChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
 }
 
