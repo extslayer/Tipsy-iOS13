7bc3e2fc3723c052f76f593f76f99fa5db943562
extslayer <kabirismanmohan@gmail.com>
2024-11-13 19:10:04 +0530

    added calculation brain

--- Tipsy/ResultViewController.swift
+++ Tipsy/ResultViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -15,18 +15,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var tenPcButton: UIButton!
     @IBOutlet weak var twentyPcButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    var tipPc: Double = 0.0
+    var splitNumber: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func tipsChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        
+        switch sender.titleLabel?.text {
+        case zeroPcButton.titleLabel?.text:
+            tipPc = 0.0
+            zeroPcButton.isSelected = true
+            tenPcButton.isSelected = false
+            twentyPcButton.isSelected = false
+        case tenPcButton.titleLabel?.text:
+            tipPc = 0.1
+            zeroPcButton.isSelected = false
+            tenPcButton.isSelected = true
+            twentyPcButton.isSelected = false
+        case twentyPcButton.titleLabel?.text:
+            tipPc = 0.2
+            zeroPcButton.isSelected = false
+            tenPcButton.isSelected = false
+            twentyPcButton.isSelected = true
+        default:
+            tenPcButton.isSelected = true
+        }
+        
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumber = Int(sender.value)
+        splitNumberLabel.text = "\(splitNumber)"
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        let billAmount = Double(billTextField.text ?? "0.0")
+        print(tipPc)
+        print(splitNumber)
+        print(billAmount ?? "0.0")
+        print(calculateSplit(billAmt: billAmount!))
+        
+    }
+    
+    func calculateSplit(billAmt:Double)->Double{
+        let tipAmount = billAmt * tipPc
+        let totalAmount = billAmt + tipAmount
+        let totalSplitAmt = totalAmount / Double(splitNumber)
+        return totalSplitAmt
     }
 }
 
